1d0e9af9a8485dba2c524b180c186719334bd9a7
任柏勳 <rick8220602@gmail.com>
2019-02-14 22:51:56 +0800

    ArrayDeclare

---
+++ src/collections/ArrayDeclare.java
@@ -0,0 +1,18 @@
+package collections;
+
+public class ArrayDeclare {
+
+    public static void main(String[] args) {
+
+        // 一樣都是一維陣列
+        int[] arr1;
+        int arr2[];
+
+        // 正常情況
+//        int i;
+//        int[] arr;
+
+        // 一行的情況
+//        int i, arr[];
+    }
+}
