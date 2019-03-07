a0f7d19cb057faafc01396e7cc23f19edef89cce
任柏勳 <rick8220602@gmail.com>
2019-03-07 22:40:57 +0800

    Complement

--- src/operators/Shift.java
+++ src/operators/Shift.java
@@ -12,6 +12,9 @@ public class Shift {
         System.out.println(1 << 2); // 4
         System.out.println(1 << 3); // 8
 
+        System.out.println(1 << 34); // 4
+        System.out.println(1 << 66); // 4
+
         // 無負號右移運算
         System.out.println(-1 >>> 1); // 2147483647
         System.out.println(-1 >>> 2); // 1073741823
